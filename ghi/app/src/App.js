ff76d99f200401b5e3c6c2517d889204077f9fc7
Alex Reiman <reiman.alex@gmail.com>
2022-12-06 12:18:02 -0700

    imported Manufacturer Form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturersList from './ManufacturersList';
+import ManufacturerForm from './ManufacturerForm';
 
 function App() {
   return (
