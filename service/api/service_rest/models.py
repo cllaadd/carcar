a45ea497458f6e1c863dc790b880571a7a3e13d3
Claudia Harrison <claudia3695@gmail.com>
2022-12-06 18:18:01 -0600

    created and registered technician model and made create and list view for it

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# class VinVO(models.model):
+#     id = models.CharField(max_length=17, unique=True)
+
+# class Status(models.Model):
+#     id = models.PositiveSmallIntegerField(primary_key=True)
+#     name = models.CharField(max_length=10, unique=True)
+
+#     def __str__(self):
+#         return self.name
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.PositiveSmallIntegerField(primary_key=True)
+
+    class Meta:
+        ordering = ("id",)
+
+
+# class Appointment(models.Model):
+#     owner = models.CharField(max_length=100)
+#     date = models.DateField(_(""), auto_now=False, auto_now_add=False)
+#     time = models.TimeField()
+#     reason = models.CharField(max_length=200)
+
+#     vin = models.ForeignKey(
+#         VinVO,
+#         related_name="appointments",
+#         on_delete=models.CASCADE,
+#     )
+
+#     status = models.ForeignKey(
+#         Status,
+#         related_name="appointments",
+#         on_delete=models.PROTECT,
+#     )
+
+#     class Meta:
+#         ordering = ("date",)
